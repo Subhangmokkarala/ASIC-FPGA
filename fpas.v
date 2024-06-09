27125a06d5cca643e7eafb89064da5395504163a
Subhang Mokkarala <90596655+Subhangmokkarala@users.noreply.github.com>
2024-06-09 22:38:49 +0530

    Create fpas.v

---
+++ fpas.v
@@ -0,0 +1,14 @@
+// File: matrix_multiply.v
+module matrix_multiply (
+    input clk,
+    input rst,
+    input start,
+    input [31:0] A [0:3][0:3],
+    input [31:0] B [0:3][0:3],
+    output reg [31:0] C [0:3][0:3],
+    output reg done
+);
+
+// RTL code for matrix multiplication which i really dont know 
+
+endmodule
